11cf52b09e127db8ac0f98e3e2349af689d7e954
sergio <gioserman19@gmail.com>
2026-03-06 09:07:25 -0500

    correcion de bite config otra vez

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    // Elimina 'minify: false' o cámbialo a true para producción
-    minify: true, 
+    minify: true,
     outDir: 'dist',
-  },
-  // './' ayuda a que funcione en subcarpetas en cPanel
-  base: './' 
+  }
 })
